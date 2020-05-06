3aa63f0112d0a3614209fff42c5c92ce6adc2919
TDICoder <60338973+TDICoder@users.noreply.github.com>
2020-05-06 15:29:42 +0500

    Day 2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
+
+//1. Получаем элементы 
+//2. Функции
+//3. События 
+//4. Запуск
+
+// 0.КОНСТАНТЫ////////////////////////////////////////////////////////////////////////
+const BIRTHDAY = '06.05.2020';
+
+// 1.ПЕРЕМЕННЫЕ/////////////////////////////////////////////////////////////////////////
+// 0
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
-
-cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
-
-function toggleModal() {
-  modal.classList.toggle("is-open");
-}
-
-//Day 1
-
-//1.Получаем элементы 
-//2. Функции
-//2. Навешиваем события 
-
-// ПЕРЕМЕННЫЕ/////////////////////////////////////////////////////////////////////////
+// 1
 const buttonAuth = document.querySelector(".button-auth");
 const modalAuth = document.querySelector(".modal-auth");
 const closeModalAuth = document.querySelector(".close-auth");
@@ -24,6 +21,15 @@ const loginInput = document.querySelector("#login");
 const loginPass = document.querySelector("#password")
 const userName = document.querySelector(".user-name")
 const buttonOut = document.querySelector(".button-out")
+//2
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+
+const containerPromo = document.querySelector('.container-promo'); // скрыть, при клике на ресторан 
+const restaurants = document.querySelector('.restaurants') // скрыть, при клике на ресторан 
+const menu = document.querySelector('.menu'); // показать, при клике на ресторан 
+const logo = document.querySelector('.logo');
+
+const cardsMenu = document.querySelector('.cards-menu');
 
 //console.log(buttonLogin);
 
@@ -41,10 +47,17 @@ login = localStorage.getItem('delivery');
 
 //console.log(logInForm);
 
-// ФУНКЦИИ////////////////////////////////////////////////////////////////////////////
+// 2.ФУНКЦИИ////////////////////////////////////////////////////////////////////////////
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
+
 // Скрыть/показать модальное окно Авторизации
 function toggleModalAuth()  {
   modalAuth.classList.toggle('is-open');
+  loginInput.placeholder = '';loginPass.placeholder = '';loginInput.style.borderColor = '';loginPass.style.borderColor = '';
+  loginInput.value = loginInput.value.trim();
+  loginPass.value = loginPass.value
 }
 
 //Если не Авторизован
@@ -52,13 +65,16 @@ function notAuthorized () {
 
   console.log('не авторизован');
 
+    // Скрываем корзину
+    cartButton.style.display = 'none';
+
           function logIn(event){
             //console.log('логин');
             // Выключаем отправку формы по submit
             event.preventDefault();  
             
-            login = loginInput.value;
-            password = loginPass.value;
+            login = loginInput.value
+            password = loginPass.value.trim();
 
             localStorage.setItem('delivery',login); // Сохраняем значение инпут в локалсторадж
             //console.log(login);
@@ -66,7 +82,6 @@ function notAuthorized () {
             //  debugger;
            
             if (login && password ) {
-              loginInput.placeholder = '';loginPass.placeholder = '';
               toggleModalAuth();
               buttonAuth.removeEventListener("click",toggleModalAuth)
               closeModalAuth.removeEventListener("click",toggleModalAuth);
@@ -75,8 +90,8 @@ function notAuthorized () {
               checkAuth();
             }
             else {
-              if(!login) { loginInput.placeholder = 'Обязательное поле'; loginInput.focus(); return;}
-              if(!password) { loginPass.placeholder = 'Обязательное поле'; loginPass.focus(); }
+              if(!login) { loginInput.placeholder = 'Обязательное поле';loginInput.style.borderColor = 'red'; loginInput.focus(); return;}
+              if(!password) { loginPass.placeholder = 'Обязательное поле';loginPass.style.borderColor = 'red'; loginPass.focus(); }
             }
           }
 
@@ -85,14 +100,17 @@ function notAuthorized () {
   logInForm.addEventListener("submit",logIn);
 }
 
-
 //Если Авторизован
 function authorized () {
-
     console.log('aвторизован');
     //buttonAuth.style.backgroundColor = 'red';
 
         function logOut(){
+
+          // Возврат на главную
+          desc();
+          ////////////////////////////////
+
           login = null;  password = null;
           buttonAuth.style.display = '';
           userName.style.display = '';
@@ -112,6 +130,9 @@ function authorized () {
 
   buttonOut.addEventListener("click",logOut);
 
+  // Показываем корзину
+  cartButton.style.display = 'flex';
+
 }
 
 function checkAuth() {
@@ -123,6 +144,129 @@ function checkAuth() {
   }
 
 
+function createCardRestaurant() {
+
+  // Формируем верстку ресторана
+  const card = `
+      <a class="card card-restaurant">
+      <img src="img/pizza-plus/preview.jpg" alt="image" class="card-image"/>
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title">Пицца плюс</h3>
+          <span class="card-tag tag">50 мин</span>
+        </div>
+        <div class="card-info">
+          <div class="rating">
+            4.5
+          </div>
+          <div class="price">От 900 ₽</div>
+          <div class="category">Пицца</div>
+        </div>
+      </div>
+    </a>
+  `;
+  // Втавляем в верстку
+  cardsRestaurants.insertAdjacentHTML('beforeend',card);
+}
+
+
+
+function createCardGood() {
+  // Создаем оболочку для верстки товара и добаялем класс  card
+  const cardGood = document.createElement('div');
+  cardGood.className = 'card';
+  // сама верстка блюда
+  const good =  `
+        <img src="img/pizza-plus/pizza-classic.jpg" alt="image" class="card-image"/>
+        <div class="card-text">
+          <div class="card-heading">
+            <h3 class="card-title card-title-reg">Пицца Классика</h3>
+          </div>
+          <div class="card-info">
+            <div class="ingredients">Соус томатный, сыр «Моцарелла», сыр «Пармезан», ветчина, салями,
+              грибы.
+            </div>
+          </div>
+          <div class="card-buttons">
+            <button class="button button-primary button-add-cart">
+              <span class="button-card-text">В корзину</span>
+              <span class="button-cart-svg"></span>
+            </button>
+            <strong class="card-price-bold">510 ₽</strong>
+          </div>
+        </div>
+      `;
+  // Добавляем в ДОМ
+  cardGood.insertAdjacentHTML('beforeend',good);
+
+  cardsMenu.insertAdjacentElement('beforeend',cardGood);
+
+  // console.log(cardGood);
+
+}
+
+
+function openGoods (event) {
+  // определяем куда конкретно кликнули
+  const target = event.target;
+  // определяем верхнюю оболчку карточки,closest поднимается выше до селектора (класса) cards-restaurants
+  const restaurant = target.closest('.card-restaurant');
+  //  console.log('restaurant',restaurant);
+
+  // скрываем все рестораны, показываем меню
+  if(restaurant) {
+
+    // Очищем блюда в ресторане
+    cardsMenu.textContent = '';
+
+    // Проверка на авторизацию
+    if(!login){
+      toggleModalAuth();
+    }
+    //////////////////////////
+
+    if(login) {
+        containerPromo.classList.add('hide');
+        restaurants.classList.add('hide');
+        menu.classList.remove('hide');
+
+        createCardGood();
+        createCardGood();
+        createCardGood();
+      }
+
+  }
+
+}
+
+// Возврат на главную-выход
+function desc() {
+  // Очищем блюда в ресторане
+  cardsMenu.textContent = '';
+
+  containerPromo.classList.remove('hide');
+  restaurants.classList.remove('hide');
+  menu.classList.add('hide');
+}
+
+
+// 3.ОБРАБОТЧИКИ СОБЫТИЙ///////////////////////////////////////////////////
 // Запуск, СОБЫТИЯ////////////////////////////////////////////////////////////////////////////
 
+// Клик на корзину
+cartButton.addEventListener("click", toggleModal);
+// Клик на закрытие карзины
+close.addEventListener("click", toggleModal);
+// Клик на ресторан
+cardsRestaurants.addEventListener('click',openGoods)
+
+// При клике на лого выозвращяем рестораны
+logo.addEventListener('click', desc)
+
+// debugger;
+
 checkAuth();
+
+createCardRestaurant();
+createCardRestaurant();
+createCardRestaurant();
